f29b7f038236c88ac0ec2b9bb10c2aeee214bb11
Travis CI <travis@travis-ci.org>
2018-03-24 23:48:11 +0100

    Add install-osx-deps-32bit.sh

---
+++ install-osx-deps-32bit.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+brew install caskroom/cask/xquartz --32-bit
+brew install --only-dependencies caskroom/versions/wine-devel --32-bit
+brew install gettext --32-bit
+brew install libquicktime --32-bit
+brew install libgphoto2 --32-bit
+brew install libgsm --32-bit
+brew install gst-plugins-base --32-bit
+brew install openal-soft --32-bit
