d3bd010153c6f1229248fb75577d9bbc7923e2e3
Travis CI <travis@travis-ci.org>
2018-03-25 00:02:20 +0100

    Try --universal

--- install-osx-deps-32bit.sh
+++ install-osx-deps-32bit.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-brew install caskroom/cask/xquartz --32-bit
-brew install --only-dependencies caskroom/versions/wine-devel --32-bit
-brew install gettext --32-bit
-brew install libquicktime --32-bit
-brew install libgphoto2 --32-bit
-brew install libgsm --32-bit
-brew install gst-plugins-base --32-bit
-brew install openal-soft --32-bit
+brew install caskroom/cask/xquartz --universal
+brew install --only-dependencies caskroom/versions/wine-devel --universal
+brew install gettext --universal
+brew install libquicktime --universal
+brew install libgphoto2 --universal
+brew install libgsm --universal
+brew install gst-plugins-base --universal
+brew install openal-soft --universal
