20c01e6f766ec0dc9b5a6e1d9fd58479a8f505bd
Travis CI <travis@travis-ci.org>
2018-03-24 23:34:00 +0100

    Add install-osx-deps.sh

---
+++ install-osx-deps.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+brew install caskroom/cask/xquartz
+brew install --only-dependencies caskroom/versions/wine-devel
+brew install gettext
+brew install libquicktime
+brew install libgphoto2
+brew install libgsm
+brew install gst-plugins-base
+brew install openal-soft
