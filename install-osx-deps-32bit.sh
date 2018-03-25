e154c88abc835284cb06363f5bff091423fa23aa
Travis CI <travis@travis-ci.org>
2018-03-25 13:24:03 +0200

    Use fully qualified brew names

--- install-osx-deps-32bit.sh
+++ install-osx-deps-32bit.sh
@@ -3,10 +3,11 @@
 brew tap mistydemeo/tigerbrew https://github.com/mistydemeo/tigerbrew
 brew tap-pin mistydemeo/tigerbrew
 
-brew install freetype --universal
-brew install gettext --universal
-brew install libquicktime --universal
-brew install libgphoto2 --universal
-brew install libgsm --universal
-brew install gst-plugins-base --universal
-brew install openal-soft --universal
+brew install mistydemeo/tigerbrew/freetype --universal
+brew remove gettext
+brew install mistydemeo/tigerbrew/gettext --universal
+brew install mistydemeo/tigerbrew/libquicktime --universal
+brew install mistydemeo/tigerbrew/libgphoto2 --universal
+brew install mistydemeo/tigerbrew/libgsm --universal
+brew install mistydemeo/tigerbrew/gst-plugins-base --universal
+brew install mistydemeo/tigerbrew/openal-soft --universal
