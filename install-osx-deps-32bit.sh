56361548a720d0ed504737d7f5b15a1be2ee514b
Travis CI <travis@travis-ci.org>
2018-03-25 10:49:07 +0200

    Install freetype

--- install-osx-deps-32bit.sh
+++ install-osx-deps-32bit.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 brew install caskroom/cask/xquartz --universal
 brew install --only-dependencies caskroom/versions/wine-devel --universal
+brew install freetype --universal
 brew install gettext --universal
 brew install libquicktime --universal
 brew install libgphoto2 --universal
