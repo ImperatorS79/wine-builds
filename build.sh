8207199e13cdf96e4aaa9c7c9064ed2aa4d4ba9f
ImperatorS79 <fevrier.simon@gmail.com>
2018-05-28 12:00:02 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+wget https://dl.winehq.org/vkd3d/source/vkd3d-1.0.tar.xz
+tar -xf vkd3d-1.0.tar.xz
+mkdir build_vkd3d &&
+cd build_vkd3d &&
+../vkd3d-1.0/configure $1 &&
+make -j2 &&
+make install
+
+cd ../
 git clone https://github.com/wine-mirror/wine.git
 mkdir build &&
 cd build &&
