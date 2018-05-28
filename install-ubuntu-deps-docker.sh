6cdec2a71e5ab35c8398deb22bd5b05fa8f5d163
ImperatorS79 <fevrier.simon@gmail.com>
2018-05-28 11:58:17 +0200

    Update install-ubuntu-deps-docker.sh

--- install-ubuntu-deps-docker.sh
+++ install-ubuntu-deps-docker.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 docker exec -t ubuntu-16-04 bash -c "apt-get -qq update;
                                      apt-get install -y git;
+                                     apt-get install -y build-essential;
                                      apt-get install -y tree;
                                      apt-get install -y flex;
                                      apt-get install -y bison;
@@ -40,4 +41,13 @@ docker exec -t ubuntu-16-04 bash -c "apt-get -qq update;
                                      apt-get install -y libxslt1-dev;
                                      apt-get install -y libgnutls-dev;
                                      apt-get install -y libjpeg-dev;
-                                     apt-get install -y libvulkan-dev"
+                                     apt-get install -y oss4-dev;
+                                     apt-get install -y wget;
+                                     apt-get install -y pkg-config;
+				                             apt-get install -y libxcb-keysyms1-dev;
+				                             apt-get install -y libxcb-util0-dev;
+				                             apt-get install -y libxcb-icccm4-dev;
+                                     apt-get install -y libvulkan-dev;
+				                             wget https://launchpad.net/ubuntu/+archive/primary/+sourcefiles/spirv-headers/1.3-1/spirv-headers_1.3.orig.tar.gz ;
+				                             tar -zxvf spirv-headers_1.3.orig.tar.gz;
+				                             mv spirv-headers-1.3/include/spirv /usr/include"
