69246ab95f518572bd933cac201fff10cb96a5e1
ImperatorS79 <fevrier.simon@gmail.com>
2018-05-28 12:06:27 +0200

    Update build-staging.sh

--- build-staging.sh
+++ build-staging.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
+wget https://dl.winehq.org/vkd3d/source/vkd3d-1.0.tar.xz
+tar -xf vkd3d-1.0.tar.xz
+mkdir build_vkd3d &&
+cd build_vkd3d &&
+../vkd3d-1.0/configure $1 &&
+make -j2 &&
+make install
+
+cd ../
+
 git clone https://github.com/wine-mirror/wine.git
-# TBD: use https://github.com/alesliehughes/alesliehughes-wine-staging.git
-git clone https://github.com/wine-compholio/wine-staging.git
+git clone https://github.com/wine-wine-staging/wine-staging.git
 ./wine-staging/patches/patchinstall.sh DESTDIR=wine --all
 mkdir build &&
 cd build &&
