d438120bb22ad5e83d9f2f519db16bdb87923680
ImperatorS79 <fevrier.simon@gmail.com>
2018-05-30 17:20:56 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ tar -xf vkd3d-1.0.tar.xz
 mkdir build_vkd3d &&
 cd build_vkd3d &&
 ../vkd3d-1.0/configure --with-spirv-tools $1 &&
-make -j2 &&
+make -j3 &&
 make install
 
 cd ../
@@ -12,4 +12,4 @@ git clone https://github.com/wine-mirror/wine.git
 mkdir build &&
 cd build &&
 ../wine/configure $1 &&
-make -j2
+make -j3
