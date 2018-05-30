566f32bf9f75c82ab34aaeeb69784c2c24876d59
ImperatorS79 <fevrier.simon@gmail.com>
2018-05-30 15:59:32 +0200

    Build with spirv-tools

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ wget https://dl.winehq.org/vkd3d/source/vkd3d-1.0.tar.xz
 tar -xf vkd3d-1.0.tar.xz
 mkdir build_vkd3d &&
 cd build_vkd3d &&
-../vkd3d-1.0/configure $1 &&
+../vkd3d-1.0/configure --with-spirv-tools $1 &&
 make -j2 &&
 make install
 
