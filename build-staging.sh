b7d87f0a998cc36ab660d944f1f8fa18c69312c9
ImperatorS79 <fevrier.simon@gmail.com>
2018-06-08 19:37:49 +0200

    Update build-staging.sh

--- build-staging.sh
+++ build-staging.sh
@@ -11,7 +11,9 @@ cd ../
 wget https://dl.winehq.org/wine/source/3.x/wine-3.9.tar.xz
 tar -xf wine-3.9.tar.xz
 git clone -n https://github.com/wine-staging/wine-staging.git
+cd wine-staging
 git reset --hard 69650f38d8979fc40ccb4a4bee64c06fb59336d5
+cd ../
 ./wine-staging/patches/patchinstall.sh DESTDIR=wine-3.9 --all
 mkdir build &&
 cd build &&
