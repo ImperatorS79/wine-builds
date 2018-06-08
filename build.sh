0c8c6adecc903ca419b560c71e3cb03f8cff2ec4
ImperatorS79 <fevrier.simon@gmail.com>
2018-06-08 18:11:58 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ make -j3 &&
 make install
 
 cd ../
-https://dl.winehq.org/wine/source/3.x/wine-3.9.tar.xz
+wget https://dl.winehq.org/wine/source/3.x/wine-3.9.tar.xz
 tar -xf wine-3.9.tar.xz
 mkdir build &&
 cd build &&
