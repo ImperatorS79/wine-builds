5e9004111bffc3bead6d64b614d43594a321f81c
ImperatorS79 <fevrier.simon@gmail.com>
2018-06-09 13:50:24 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,4 @@
 #!/bin/sh
-wget https://dl.winehq.org/vkd3d/source/vkd3d-1.0.tar.xz
-tar -xf vkd3d-1.0.tar.xz
-mkdir build_vkd3d &&
-cd build_vkd3d &&
-../vkd3d-1.0/configure --with-spirv-tools $1 &&
-make -j3 &&
-make install
-
-cd ../
 wget https://dl.winehq.org/wine/source/3.x/wine-3.9.tar.xz
 tar -xf wine-3.9.tar.xz
 mkdir build &&
